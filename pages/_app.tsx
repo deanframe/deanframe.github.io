4241ba71cb46650619a06d49f0607dd5df926d41
Dean Frame <dacframe@gmail.com>
2022-12-20 01:28:26 -0800

    barebones but presentable

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Questrial } from '@next/font/google'
+import '../styles/globals.css'
 
 const questrial = Questrial({ subsets: ['latin'], weight: '400' })
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -7,13 +8,28 @@ export default function Home() {
         <title>Dean Frame</title>
         <meta name="description" content="Dean Frame's personal website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.svg" />
       </Head>
       <main>
-        <div>
-          Dean Frame
-        </div>
-        <div>
-          Website under construction!
+        <div className='content-container'>
+          <div className='title'>
+            Dean Frame
+          </div>
+          <div>
+            honors cs @ northeastern
+          </div>
+          <div>
+            incoming swe @ <Link target='_blank' href='https://www.stripe.com'>stripe</Link>
+          </div>
+          <div>
+            <Link href='/Resume.pdf' download>resume</Link>
+            {' | '}
+            <Link target='_blank' href='https://www.linkedin.com/in/dean-frame'>linkedin</Link>
+            {' | '}
+            <Link target='_blank' href='https://www.github.com/deanframe'>github</Link>
+            {' | '}
+            <Link href='mailto:dacframe@gmail.com'>email</Link>
+          </div>
         </div>
       </main>
     </>
