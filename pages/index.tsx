dc12359b7fea3f5f2fa3b767b1f851f448d39058
Dean Frame <dacframe@gmail.com>
2023-06-27 14:22:29 -0400

    postgrad updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,29 +18,26 @@ export default function Home() {
             Dean Frame
           </div>
           <div>
-            honors cs @ northeastern
-          </div>
-          <div>
-            incoming new grad swe @ <Link target='_blank' href='https://www.stripe.com'>stripe</Link>
+            incoming software engineer @ <Link className='stripe' target='_blank' href='https://www.stripe.com'>stripe</Link>
           </div>
           <div>
             previous experience:
             <ul>
-              <li>- intern @ <Link target='_blank' href='https://www.stripe.com'>stripe</Link> - typescript, flow js, react, graphql, grpc, ruby, java</li>
-              <li>- co-op @ <Link target='_blank' href='https://www.notability.com'>ginger labs</Link> - typescript, react, next.js, graphql, node.js, postgres</li>
-              <li>- co-op @ <Link target='_blank' href='https://www.wayfair.com'>wayfair</Link> - javascript, react, php, sql server</li>
+              <li>- intern @ <Link className='stripe' target='_blank' href='https://www.stripe.com'>stripe</Link> - typescript, flow js, react, graphql, grpc, ruby, java</li>
+              <li>- co-op @ <Link className='notability' target='_blank' href='https://www.notability.com'>ginger labs</Link> - typescript, react, next.js, graphql, node.js, postgres</li>
+              <li>- co-op @ <Link className='wayfair' target='_blank' href='https://www.wayfair.com'>wayfair</Link> - javascript, react, php, sql server</li>
             </ul>
           </div>
           <div>
-            check out <Link target='_blank' href='https://www.hackbeanpot.com'>hackbeanpot</Link>, a hackathon i{"'"}ve been helping organize this past year!</div>
+            check out <Link className='hackbeanpot' target='_blank' href='https://www.hackbeanpot.com'>hackbeanpot</Link>, a Boston-area hackathon I helped organize while in school!</div>
           <div>
-            <Link target='_blank' href='/Resume.pdf' download>resume</Link>
+            <Link className='link' target='_blank' href='/Resume.pdf' download>resume</Link>
             {' | '}
-            <Link target='_blank' href='https://www.linkedin.com/in/dean-frame'>linkedin</Link>
+            <Link className='link' target='_blank' href='https://www.linkedin.com/in/dean-frame'>linkedin</Link>
             {' | '}
-            <Link target='_blank' href='https://www.github.com/deanframe'>github</Link>
+            <Link className='link' target='_blank' href='https://www.github.com/deanframe'>github</Link>
             {' | '}
-            <Link href='mailto:dacframe@gmail.com'>email</Link>
+            <Link className='link' href='mailto:dacframe@gmail.com'>email</Link>
           </div>
         </div>
       </main>
